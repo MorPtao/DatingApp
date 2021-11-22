ce4dc1646a842623c69e479bc8fc8a25db06a7c2
MorPinto <morpato@gmail.com>
2021-11-22 10:31:24 +0200

    End of section 2

---
+++ server.js
@@ -0,0 +1,20 @@
+// const express = require('express');
+// const app = express();
+
+
+// app.get('/api/users',(req,res)=>{
+//     res.json([
+//         {
+//             id:1,
+//             username:'Francis'
+//         },
+//         {
+//             id:1,
+//             username:'Steve'
+//         }
+//     ])
+// })
+
+
+// const port = process.env.PORT || 3001;
+// app.listen(port)
